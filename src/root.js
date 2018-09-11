38af833e28a7850a1621826e90ce11a973ec9d6b
YJ <1553937164@qq.com>
2018-09-11 23:45:27 +0800

    list and fetch

--- src/root.js
+++ src/root.js
@@ -1,42 +1,96 @@
-import React, { Component } from 'react';
-import { SectionList, StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { Image, FlatList, StyleSheet, Text, View } from "react-native";
+
+var REQUEST_URL =
+  "https://raw.githubusercontent.com/facebook/react-native/0.51-stable/docs/MoviesExample.json";
 
 export default class Root extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: [],
+      loaded: false
+    }
+    this.fetchData = this.fetchData.bind(this)
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then(response => response.json())
+      .then(responseData => {        // 注意，这里使用了this关键字，为了保证this在调用时仍然指向当前组件，我们需要对其进行“绑定”操作
+        this.setState({
+          data: this.state.data.concat(responseData.movies),
+          loaded: true
+        })
+      })
+  }
+
   render() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView()
+    }
+    return (
+      <FlatList
+        data={this.state.data}
+        renderItem={this.renderMovie}
+        style={styles.list}
+      />
+    )
+  }
+
+  renderLoadingView() {
     return (
       <View style={styles.container}>
-        <SectionList
-          sections={[
-            {title: 'D', data: ['Devin']},
-            {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
-          ]}
-          renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
-          renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
-          keyExtractor={(item, index) => index}
+        <Text>Loading movies...</Text>
+      </View>
+    )
+  }
+
+  renderMovie({ item }) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: item.posters.thumbnail }}
+          style={styles.thumbnail}
         />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{item.title}</Text>
+          <Text style={styles.year}>{item.year}</Text>
+        </View>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
   container: {
-   flex: 1,
-   paddingTop: 22
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
-  sectionHeader: {
-    paddingTop: 2,
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingBottom: 2,
-    fontSize: 14,
-    fontWeight: 'bold',
-    color: '#fff',
-    backgroundColor: '#222',
+  rightContainer: {
+    flex: 1
   },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: "center"
   },
-})
+  year: {
+    textAlign: "center"
+  },
+  thumbnail: {
+    width: 53,
+    height: 81
+  },
+  list: {
+    paddingTop: 20,
+    backgroundColor: "#F5FCFF"
+  }
+});
